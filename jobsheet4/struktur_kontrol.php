020bd718b55364daf6bf18b840020632514eedfd
nuralfiyanti <nuralfiyanti05@gmail.com>
2025-09-21 19:32:30 +0700

    Praktikum 4 Penggunaan Struktur Kontrol pada PHP soal no 4.4

--- jobsheet4/struktur_kontrol.php
+++ jobsheet4/struktur_kontrol.php
@@ -38,4 +38,15 @@ for ($i = 1; $i <= $jumlahLahan; $i++) {
 
 echo "Jumlah buah yang akan dipanen adalah: $jumlahBuah";
 
+echo "<hr>";
+
+$skorUjian = [85, 92, 78, 96, 88];
+$totalSkor = 0;
+
+foreach ($skorUjian as $skor) {
+    $totalSkor += $skor;
+}
+
+echo "Total skor ujian adalah: $totalSkor";
+
 ?>
