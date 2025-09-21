3e3f2cb4862bcc3bdf2534548b8f8bdda4a30961
nuralfiyanti <nuralfiyanti05@gmail.com>
2025-09-21 20:53:53 +0700

    Praktikum 5 penggunaan Array soal 5.1

---
+++ jobsheet4/array.php
@@ -0,0 +1,14 @@
+<?php
+echo "<h3>Soal 5.1</h3>";
+
+$nilaiSiswa = [85, 92, 78, 64, 90, 55, 88, 79, 70, 96];
+$nilaiLulus = [];
+
+foreach ($nilaiSiswa as $nilai) {
+    if ($nilai >= 70) {
+        $nilaiLulus[] = $nilai;
+    }
+}
+
+echo "Daftar nilai siswa yang lulus: " . implode(', ', $nilaiLulus) . "<br>";
+?>
