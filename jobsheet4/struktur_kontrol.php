ec54588595a4888f04d34f555eca4764021106de
nuralfiyanti <nuralfiyanti05@gmail.com>
2025-09-21 19:40:24 +0700

    Praktikum 4 Penggunaan Struktur Kontrol pada PHP soal no 4.5

--- jobsheet4/struktur_kontrol.php
+++ jobsheet4/struktur_kontrol.php
@@ -49,4 +49,16 @@ foreach ($skorUjian as $skor) {
 
 echo "Total skor ujian adalah: $totalSkor";
 
+echo "<hr>";
+
+$nilaiSiswa = [85, 92, 58, 64, 90, 55, 88, 79, 70, 96];
+
+foreach ($nilaiSiswa as $nilai) {
+    if ($nilai < 60) {
+        echo "Nilai: $nilai (Tidak lulus) <br>";
+        continue;
+    }
+    echo "Nilai: $nilai (Lulus) <br>";
+}
+
 ?>
