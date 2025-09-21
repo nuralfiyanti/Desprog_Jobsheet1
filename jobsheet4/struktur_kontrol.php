7446d4e9d00eee6335ff90b319050f21f168fdef
nuralfiyanti <nuralfiyanti05@gmail.com>
2025-09-21 18:38:45 +0700

    Praktikum 4 Penggunaan Struktur Kontrol Pada PHP soal no 4.3

--- jobsheet4/struktur_kontrol.php
+++ jobsheet4/struktur_kontrol.php
@@ -25,4 +25,17 @@ while ($jarakSaatIni < $jarakTarget) {
 
 echo "Atlet tersebut memerlukan $hari hari untuk mencapai jarak 500 kilometer.";
 
+echo "<hr>";
+
+$jumlahLahan = 10;
+$tanamanPerLahan = 5;
+$buahPerTanaman = 10;
+$jumlahBuah = 0;
+
+for ($i = 1; $i <= $jumlahLahan; $i++) {
+    $jumlahBuah += ($tanamanPerLahan * $buahPerTanaman);
+}
+
+echo "Jumlah buah yang akan dipanen adalah: $jumlahBuah";
+
 ?>
