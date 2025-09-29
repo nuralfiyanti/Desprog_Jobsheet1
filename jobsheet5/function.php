960e239b177fc4174c34d9e0906ec64fdf28a9e4
nuralfiyanti <nuralfiyanti05@gmail.com>
2025-09-30 06:55:33 +0700

    jobsheet5 - function.php soal no 6

--- jobsheet5/function.php
+++ jobsheet5/function.php
@@ -1,19 +1,19 @@
 <?php
-// membuat fungsi 
-function perkenalan($nama, $salam) {
-    echo $salam . ", ";
-    echo "Perkenalkan, nama saya " . $nama . "<br/>";
-    echo "Senang berkenalan dengan Anda<br/><br/>";
+//membuat fungsi
+function perkenalan ($nama, $salam="Assalamualaikum"){
+    echo $salam.", ";
+    echo "Perkenalkan, nama saya ".$nama."<br/>";
+    echo "Senang berkenalan dengan Anda<br/>";
 }
 
-// memanggil fungsi yang sudah dibuat
-perkenalan("Hamdana", "Hallo");
+//memanggil fungsi yang sudah dibuat
+perkenalan ("Hamdana", "Hallo");
 
 echo "<hr>";
 
 $saya = "Elok";
 $ucapanSalam = "Selamat pagi";
 
-// memanggil lagi
-perkenalan($saya, $ucapanSalam);
+//memanggil lagi tanpa mengisi parameter salam
+perkenalan ($saya);
 ?>
