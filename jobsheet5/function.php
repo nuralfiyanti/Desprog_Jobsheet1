ffc1dd5a4567f81271ef24d47de2b08c57cefecd
nuralfiyanti <nuralfiyanti05@gmail.com>
2025-09-30 06:47:43 +0700

    jobsheet5 - function.php soal no 5

--- jobsheet5/function.php
+++ jobsheet5/function.php
@@ -1,13 +1,19 @@
 <?php
-
-function perkenalan(){
-    echo "Assalamualaikum, ";
-    echo "Perkenalkan, nama saya Nur Alfiyanti<br/>";
+// membuat fungsi 
+function perkenalan($nama, $salam) {
+    echo $salam . ", ";
+    echo "Perkenalkan, nama saya " . $nama . "<br/>";
     echo "Senang berkenalan dengan Anda<br/><br/>";
 }
 
-// memanggil fungsi yang sudah dibuat (dua kali)
-perkenalan();
-perkenalan();
+// memanggil fungsi yang sudah dibuat
+perkenalan("Hamdana", "Hallo");
 
+echo "<hr>";
+
+$saya = "Elok";
+$ucapanSalam = "Selamat pagi";
+
+// memanggil lagi
+perkenalan($saya, $ucapanSalam);
 ?>
