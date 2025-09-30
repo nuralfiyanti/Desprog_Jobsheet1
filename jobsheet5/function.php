45a108048e3ecc8349f34f72646d76b1ba030b77
nuralfiyanti <nuralfiyanti05@gmail.com>
2025-09-30 07:30:52 +0700

    jobsheet5 - function.php soal 4

--- jobsheet5/function.php
+++ jobsheet5/function.php
@@ -1,16 +1,10 @@
 <?php
-//membuat fungsi
-function hitungUmur($thn_lahir, $thn_sekarang) {
-    $umur = $thn_sekarang $thn_lahir;
-    return $umur;
-}
-function perkenalan ($nama, $salam-"Assalamualaikum") {
-    echo $salam.",";
-    echo "Perkenalkan, nama saya ".$nama."<br/>";
 
-    //memanggil fungsi lain
-    echo "Saya berusia ". hitungUmur (1988, 2023)." tahun<br/>"; echo "Senang berkenalan dengan anda<br/>";
+function perkenalan(){
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Nur Alfiyanti<br/>"; //Tulis sesuai nama kalian
+    echo "Senang berkenalan dengan Anda<br/>";
 }
 
-//memanggil fungsi perkenalan
-    perkenalan ("Elok");
+//memanggil fungsi yang sudah dibuat
+perkenalan();
