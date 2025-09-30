7bffef7dce07cd02799514a80e7f7cef44a7f0fc
nuralfiyanti <nuralfiyanti05@gmail.com>
2025-09-30 09:02:26 +0700

    jobsheet5 -  multi_menu.php soal no 12

--- jobsheet5/multi_menu.php
+++ jobsheet5/multi_menu.php
@@ -25,11 +25,18 @@ $menu = [
     ],
 ];
 
-// Fungsi untuk menampilkan menu
-function tampilkanMenuBertingkat (array $menu) {
+// Fungsi rekursif untuk menampilkan menu
+function tampilkanMenuBertingkat(array $menu) {
     echo "<ul>";
-    foreach ($menu as $key => $item) {
-        echo "<li>{$item['nama']}</li>";
+    foreach ($menu as $item) {
+        echo "<li>{$item['nama']}";
+
+        // Jika ada subMenu, panggil fungsi lagi
+        if (isset($item['subMenu'])) {
+            tampilkanMenuBertingkat($item['subMenu']);
+        }
+
+        echo "</li>";
     }
     echo "</ul>";
 }
