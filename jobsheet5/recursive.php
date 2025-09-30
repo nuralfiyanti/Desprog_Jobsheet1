8d4c766dd27999824450c6b8ef2465a9318fd529
nuralfiyanti <nuralfiyanti05@gmail.com>
2025-09-30 07:55:18 +0700

    jobsheet5 - recursive.php soal no 10

--- jobsheet5/recursive.php
+++ jobsheet5/recursive.php
@@ -1,9 +1,5 @@
-<?php
-function tampilkanHaloDunia(){
-    echo "Halo dunia! <br>";
-
-    tampilkanHaloDunia();
-}
-
-tampilkanHaloDunia();
+<?php 
+for ($i=1; $i <=25; $i++) { 
+    echo "Perulangan ke-{$i} <br>"; 
+} 
 ?>
