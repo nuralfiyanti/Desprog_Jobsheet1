65a06682e8c8df64eda2073d3e2ad2c202be378b
nuralfiyanti <nuralfiyanti05@gmail.com>
2025-09-30 23:15:33 +0700

    jobsheet5 - string3.php soal 16

--- jobsheet5/string3.php
+++ jobsheet5/string3.php
@@ -1,6 +1,16 @@
 <?php
 
-$pesan = "Saya arek malang"; 
-echo strrev($pesan). "<br>";
+$pesan = "saya arek malang";
+
+# ubah variabel $pesan menjadi array dengan pemisah spasi
+$pesanPerKata = explode(" ", $pesan);
+
+# ubah setiap kata dalam array menjadi kebalikannya
+$pesanPerKata = array_map(fn($kata) => strrev($kata), $pesanPerKata);
+
+# gabungkan kembali array menjadi string dengan spasi
+$pesan = implode(" ", $pesanPerKata);
+
+echo $pesan . "<br>";
 
 ?>
