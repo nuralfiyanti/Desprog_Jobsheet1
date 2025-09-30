a62a94654c068171c822f5a43149fbee840afdc1
nuralfiyanti <nuralfiyanti05@gmail.com>
2025-09-30 08:31:23 +0700

    jobsheet5 - tambah multi_menu.php

---
+++ jobsheet5/multi_menu.php
@@ -0,0 +1,35 @@
+<?php
+$menu = [
+    [
+        "nama" => "Beranda"
+    ],
+    [
+        "nama" => "Berita",
+        "subMenu" => [
+            [
+                "nama" => "Wisata",
+                "subMenu" => [
+                    [ 
+                       "nama" => "Pantai" 
+                    ],
+                    [  
+                        "nama" => "Gunung" 
+                    ]
+                  ]
+               ],
+                [ 
+                    "nama" => "Kuliner" 
+                ],
+                [   
+                    "nama" => "Hiburan" 
+                ]
+            ]
+        ],
+        [
+            "nama" => "Tentang"
+        ],
+        [
+            "nama" => "Kontak"
+        ],
+    ];
+?>
