2af41856e2fbeca50d3210cebc54459387505113
nuralfiyanti <nuralfiyanti05@gmail.com>
2025-09-30 08:33:16 +0700

    jobsheet5 -  multi_menu.php soal no 11

--- jobsheet5/multi_menu.php
+++ jobsheet5/multi_menu.php
@@ -9,27 +9,30 @@ $menu = [
             [
                 "nama" => "Wisata",
                 "subMenu" => [
-                    [ 
-                       "nama" => "Pantai" 
-                    ],
-                    [  
-                        "nama" => "Gunung" 
-                    ]
-                  ]
-               ],
-                [ 
-                    "nama" => "Kuliner" 
-                ],
-                [   
-                    "nama" => "Hiburan" 
+                    [ "nama" => "Pantai" ],
+                    [ "nama" => "Gunung" ]
                 ]
-            ]
-        ],
-        [
-            "nama" => "Tentang"
-        ],
-        [
-            "nama" => "Kontak"
-        ],
-    ];
+            ],
+            [ "nama" => "Kuliner" ],
+            [ "nama" => "Hiburan" ]
+        ]
+    ],
+    [
+        "nama" => "Tentang"
+    ],
+    [
+        "nama" => "Kontak"
+    ],
+];
+
+// Fungsi untuk menampilkan menu
+function tampilkanMenuBertingkat (array $menu) {
+    echo "<ul>";
+    foreach ($menu as $item) {
+        echo "<li>{$item['nama']}</li>";
+    }
+    echo "</ul>";
+}
+
+tampilkanMenuBertingkat($menu);
 ?>
