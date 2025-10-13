9b8bcd5a1e596dfd4132368b675dd201ad833ff2
nuralfiyanti <nuralfiyanti05@gmail.com>
2025-10-13 21:39:49 +0700

    Jobsheet 7 - Praktikum 2 Function empty  soal no 2.1

---
+++ jobsheet7/empty.php
@@ -0,0 +1,10 @@
+<?php
+
+$myArray = array(); // Array kosong
+if (empty($myArray)) {
+    echo "Array tidak terdefinisi atau kosong.";
+} else {
+    echo "Array terdefinisi dan tidak kosong.";
+}
+
+?>
