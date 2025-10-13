6385772bd767766b9317ec0b623a342d81b62855
nuralfiyanti <nuralfiyanti05@gmail.com>
2025-10-13 21:29:39 +0700

    Jobsheet 7 - Praktikum 1 Function isset() soal no 1.2

--- jobsheet7/isset.php
+++ jobsheet7/isset.php
@@ -1,6 +1,20 @@
+<?php
 $umur;
+// cek variabel $umur ada dan >= 18
 if (isset($umur) && $umur >= 18) {
     echo "Anda sudah dewasa.";
 } else {
     echo "Anda belum dewasa atau variabel 'umur' tidak ditemukan.";
 }
+
+echo "<br>"; 
+
+$data = array("nama" => "Jane", "usia" => 25);
+
+// cek elemen 'nama' ada di dalam array $data
+if (isset($data["nama"])) {
+    echo "Nama: " . $data["nama"];
+} else {
+    echo "Variabel 'nama' tidak ditemukan dalam array.";
+}
+?>
