e84fbbf5546f2073e7eafb5ea177571bf3d8c88a
nuralfiyanti <nuralfiyanti05@gmail.com>
2025-10-13 21:43:39 +0700

    Jobsheet 7 - Praktikum 2 Function empty  soal no 2.2

--- jobsheet7/empty.php
+++ jobsheet7/empty.php
@@ -7,4 +7,12 @@ if (empty($myArray)) {
     echo "Array terdefinisi dan tidak kosong.";
 }
 
+echo "<br>";
+
+if (empty($nonExistentVar)) {
+    echo "Variabel tidak terdefinisi atau kosong.";
+} else {
+    echo "Variabel terdefinisi dan tidak kosong.";
+}
+
 ?>
