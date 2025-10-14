b77129165358600c7ea132d02be8e7917d403364
nuralfiyanti <nuralfiyanti05@gmail.com>
2025-10-14 15:03:47 +0700

    Jobsheet7 - Praktikum 7 validasi -  penambahan pasword (proses validasi)

--- jobsheet7/proses_validasi.php
+++ jobsheet7/proses_validasi.php
@@ -2,26 +2,31 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nama = $_POST["nama"];
     $email = $_POST["email"];
+    $password = $_POST["password"];
     $errors = array();
 
     if (empty($nama)) {
         $errors[] = "Nama harus diisi.";
     }
 
-    // validasi email 
     if (empty($email)) {
         $errors[] = "Email harus diisi.";
     } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         $errors[] = "Format email tidak valid.";
     }
 
-    // jika ada kesalahan validasi 
+    if (empty($password)) {
+        $errors[] = "Password harus diisi.";
+    } elseif (strlen($password) < 8) {
+        $errors[] = "Password minimal 8 karakter.";
+    }
+
     if (!empty($errors)) {
         foreach ($errors as $error) {
             echo $error . "<br>";
         }
     } else {
-        echo "Data berhasil dikirim: Nama = $nama , Email = $email";
+        echo "Data berhasil dikirim: Nama = $nama , Email = $email <br> Password: [hashed]" . md5($password);
     }
 }
 ?>
