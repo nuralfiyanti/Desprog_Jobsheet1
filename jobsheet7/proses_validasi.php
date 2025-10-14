f00fc36fe09bcab9a771627d656713fac09ff39c
nuralfiyanti <nuralfiyanti05@gmail.com>
2025-10-14 12:18:00 +0700

    Jobsheet7 - Praktikum 7 validasi - proses validasi

---
+++ jobsheet7/proses_validasi.php
@@ -0,0 +1,28 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nama = $_POST["nama"];
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    $errors = array();
+
+    if (empty($nama)) {
+        $errors[] = "Nama harus diisi.";
+    }
+
+    // validasi email 
+    if (empty($email)) {
+        $errors[] = "Email harus diisi.";
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = "Format email tidak valid.";
+    }
+
+    // jika ada kesalahan validasi 
+    if (!empty($errors)) {
+        foreach ($errors as $error) {
+            echo $error . "<br>";
+        }
+    } else {
+        echo "Data berhasil dikirim: Nama = $nama , Email = $email <br> Password: [hashed]" . md5($password);
+    }
+}
+?>
