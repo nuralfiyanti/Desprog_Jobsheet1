e9d01cd4412682b2629e6174f618b4edc6425168
nuralfiyanti <nuralfiyanti05@gmail.com>
2025-10-14 13:14:20 +0700

    Jobsheet7 - Praktikum 7 validasi - update form_validasi soal no 7.2

--- jobsheet7/form_validasi.php
+++ jobsheet7/form_validasi.php
@@ -2,20 +2,49 @@
 <html>
 <head>
     <title>Form Input dengan Validasi</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
+    
     <h1>Form Input dengan Validasi</h1>
-    <form method="post" action="proses_validasi.php">
-        <label for="nama">Nama: </label>
-        <input type="text" id="nama" name="nama">
-        <br></br>
+    <form id="myForm" method="post" action="proses_validasi.php"> 
+    <label for="nama">Nama: </label>
+    <input type="text" id="nama" name="nama" value=""> 
+    <span id="nama-error" style="color: red;"></span><br><br>
+    
+    <label for="email">Email:</label>
+    <input type="text" id="email" name="email" value="">
+    <span id="email-error" style="color: red;"></span><br><br>
+    
+    <input type="submit" value="Submit">
+</form>
 
+<script>
+$(document).ready(function() {
+    $("#myForm").submit(function(event) {
+        var nama = $("#nama").val();
+        var email = $("#email").val();
+        var valid = true;
 
-        <label for="email">Email:</label>
-        <input type="text" id="email" name="email">
-        <br></br>
-     
-        <input type="submit" value="Submit">
-    </form>
+        if (nama === "") {
+            $("#nama-error").text("Nama harus diisi!");
+            valid = false;
+        }else{
+            $("#nama-error").text("");
+        }
+
+        if (email === "") {
+            $("#email-error").text("Email harus diisi.");
+            valid = false;
+        }else{
+            $("#email-error").text("");
+        }
+
+        if (!valid) {
+            event.preventDefault(); 
+        }
+    });
+});
+</script>
 </body>
 </html>
