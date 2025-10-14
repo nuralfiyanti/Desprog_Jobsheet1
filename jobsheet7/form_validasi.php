ce6c45eb1f0260819377eaa3c69cbb1265ad310d
nuralfiyanti <nuralfiyanti05@gmail.com>
2025-10-14 15:02:49 +0700

    Jobsheet7 - Praktikum 7 validasi -  penambahan pasword (form validasi) soal no 7.4

--- jobsheet7/form_validasi.php
+++ jobsheet7/form_validasi.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Form Input dengan Validasi</title>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<title>Form Input dengan Validasi</title>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 <body>
-    
-    <h1>Form Input dengan Validasi</h1>
-    <form id="myForm" method="post" action="proses_validasi.php"> 
+<h1>Form Input dengan Validasi</h1>
+
+<form id="myForm" method="post" action="proses_validasi.php"> 
     <label for="nama">Nama: </label>
     <input type="text" id="nama" name="nama" value=""> 
     <span id="nama-error" style="color: red;"></span><br><br>
@@ -15,6 +15,10 @@
     <label for="email">Email:</label>
     <input type="text" id="email" name="email" value="">
     <span id="email-error" style="color: red;"></span><br><br>
+
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password" value="">
+    <span id="password-error" style="color: red;"></span><br><br>
     
     <input type="submit" value="Submit">
 </form>
@@ -24,7 +28,13 @@ $(document).ready(function() {
     $("#myForm").submit(function(event) {
         var nama = $("#nama").val();
         var email = $("#email").val();
+        var password = $("#password").val();
+
         var valid = true;
+
+        $("#nama-error").text("");
+        $("#email-error").text("");
+        $("#password-error").text("");
 
         if (nama === "") {
             $("#nama-error").text("Nama harus diisi!");
@@ -39,6 +49,14 @@ $(document).ready(function() {
         }else{
             $("#email-error").text("");
         }
+
+        if (password=== "") {
+            $("#password-error").text("Password harus diisi.");
+            valid = false;
+        } else if (password.length < 8) {
+            $("#password-error").text("Password minimal 8 karakter.");
+            valid = false;
+        } else $("#password-error").text("");
 
         if (!valid) {
             event.preventDefault(); 
