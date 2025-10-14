7025a66d91b2eaeace3c9dc788916af7aaa49b93
nuralfiyanti <nuralfiyanti05@gmail.com>
2025-10-14 09:09:35 +0700

    Jobsheet7 - Praktikum 5 Regex Pada PHP soal no 5.4

--- jobsheet7/regex.php
+++ jobsheet7/regex.php
@@ -21,4 +21,12 @@ $text = 'I like apple pie.';
 $new_text = preg_replace($pattern, $replacement, $text);
 echo $new_text . "<br>";
 
+$pattern = '/go*d/';
+$text = 'god is good.';
+if (preg_match($pattern, $text, $matches)) {
+    echo "Cocokkan: " . $matches[0] . "<br>";
+} else {
+    echo "Tidak ada yang cocok!<br>";
+}
+
 ?>
