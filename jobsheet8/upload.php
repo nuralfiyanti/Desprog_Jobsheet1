de94e198f5393dc937e4b79d3a1e48328c67830d
nuralfiyanti <nuralfiyanti05@gmail.com>
2025-10-29 07:10:08 +0700

    Jobsheet8 praktikum 1 - ubah isi file upload.php dari soal no 3

--- jobsheet8/upload.php
+++ jobsheet8/upload.php
@@ -2,12 +2,23 @@
 if(isset($_POST["submit"])){
     $targetdir = "uploads/"; //Direktori tujuan untuk menyimpan file
     $targetfile = $targetdir. basename($_FILES["myfile"]["name"]);
+    $filetype = strtolower(pathinfo($targetfile, PATHINFO_EXTENSION));
     
-    if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)){
-        echo "File berhasil diunggah ke folder: ";
+    $allowedExtensions = array("jpg", "jpeg", "png", "gif");
+    $maxsize = 2*1024*1024; 
+
+    if (in_array($filetype, $allowedExtensions) && $_FILES["myfile"]["size"] <= $maxsize)
+    {
+
+        if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)){
+            echo "File berhasil diunggah.";
+        }
+        else{
+            echo "Gagal mengunggah file.";
+        }
     }
     else{
-        echo "Gagal mengunggah file.";
+        echo "File tidak valid atau melebihi ukuran maximum yang diizinkan";
     }
 }
 ?> 
