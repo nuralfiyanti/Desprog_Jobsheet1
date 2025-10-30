834aa8bcfe142a211b1b38e04b3273abd375f319
nuralfiyanti <nuralfiyanti05@gmail.com>
2025-10-30 10:35:56 +0700

    Jobsheet8 - Praktikum 8 tambah file sessionCreate.php pertanyaan no 20

---
+++ jobsheet8/sessionCreate.php
@@ -0,0 +1,14 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+        // Membuat variabel session
+        $_SESSION["favcolor"] = "green";
+        $_SESSION["favanimal"] = "cat";
+        echo "Session variables are set.";
+        ?>
+    </body>
+</html>
