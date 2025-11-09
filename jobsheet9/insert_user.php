7d26b6a837f31580ac05cd59ac02a8c2a32c1874
nuralfiyanti <nuralfiyanti05@gmail.com>
2025-11-09 16:00:19 +0700

    Jobsheet9 - Praktikum 1 insert_user.php ubah isi untuk soal no 3

--- jobsheet9/insert_user.php
+++ jobsheet9/insert_user.php
@@ -1,29 +1,24 @@
 <?php
 $host = "localhost";
-$username = "root";
-$password = "";
+$db_user = "root";
+$db_pass = "";
 $database = "prakwebdb";
 
-//koneksi ke database
-$connect = mysqli_connect($host, $username, $password, $database);
-
-// cek koneksi
+$connect = mysqli_connect($host, $db_user, $db_pass, $database);
 if (!$connect) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
 
-// Data yang dimasukkan
-$user = "admin";
-$pass = md5("admin"); 
+//autoincrement 
+$username_user = 'admin';
+$password_user = md5('123'); 
 
-// memasukkan data ke tabel user
-$query = "INSERT INTO user (username, password) VALUES ('$user', '$pass')";
+$query = "INSERT INTO user (username, password) VALUES ('$username_user', '$password_user')";
 
-// jalankan query
 if (mysqli_query($connect, $query)) {
     echo "Data user berhasil dimasukkan!";
 } else {
-    echo "Error: " . mysqli_error($connect);
+    echo "Error saat insert: " . mysqli_error($connect);
 }
 
 mysqli_close($connect);
