7cf0f0cd67996679f8a84ee0b5141bacfb1fe49a
nuralfiyanti <nuralfiyanti05@gmail.com>
2025-12-06 06:10:18 +0700

    Jobsheet10.2 - Praktikum3 - soal no 3.1

--- jobsheet10.2/proses.php
+++ jobsheet10.2/proses.php
@@ -23,4 +23,36 @@ if (isset($_GET['aksi']) && $_GET['aksi'] == 'tambah') {
 
     mysqli_close($koneksi);
 }
+
+elseif (isset($_GET['aksi']) && $_GET['aksi'] == 'ubah') {
+
+    if (isset($_POST['id'])) {
+
+        $id = $_POST['id'];
+        $nama = $_POST['nama'];
+        $jenis_kelamin = $_POST['jenis_kelamin'];
+        $alamat = $_POST['alamat'];
+        $no_telp = $_POST['no_telp'];
+
+        $query = "UPDATE anggota 
+                  SET nama='$nama', 
+                      jenis_kelamin='$jenis_kelamin', 
+                      alamat='$alamat', 
+                      no_telp='$no_telp'
+                  WHERE id=$id";
+
+        if (mysqli_query($koneksi, $query)) {
+            header("Location: index.php");
+            exit();
+        } else {
+            echo "Gagal mengupdate data: " . mysqli_error($koneksi);
+        }
+
+    } else {
+        echo "ID tidak valid.";
+    }
+
+    mysqli_close($koneksi);
+}
+
 ?>
